f2ebdf45e4c6690cf8a13c142c4d1f44e3e8f972
kingjessie <jessie.kinganga@gmail.com>
2023-02-20 13:01:42 +0000

    comments added, changed order of credits/author, recreated readme file

--- CreateREADME.md
+++ CreateREADME.md
@@ -1,5 +1,5 @@
 
-  # ![screenshot](images/readme.png) **Better Nutrient** 
+  # **Bette Nutrient** ✒️📃
   
   ## Table of contents
   - [Project Description](#project-description)
@@ -14,7 +14,7 @@
       - [Credits](#credits)
 
   # **Project Description**
-  Better Nutrient is a website that focuses on promoting healthy living.
+  Better Nutrient is a website that focuses on promoting healthy living. It provides users with the ability to search for a specific food of their choice, and in return, it displays the corresponding nutrients and recipe based on their input. This enables users to track their food intake to ensure they are obtaining the necessary nutrients they require daily.
 
   ## Installation
   ```
@@ -27,14 +27,14 @@ code .
 
 
   ## Usage
-  No usage provided
+  No information provided
 
   ## Screenshot
   ![screenshot](images/bn.gif)
-![screenshot](images/bn.png)
   
   ## Built with
-  * - [x] Semantic HTML5 markup 
+  * - [] Technologies: 
+ - [x] Semantic HTML5 markup 
  - [x] CSS custom properties 
  - [x] JavaScript 
  - [x] jQuery 
@@ -44,22 +44,23 @@ code .
 
   
   ## What I learned
-  Through this process, we have 
+  For the most part the newest part of building this page was learning to work in a team, take on smaller pieces of the whole, the process of give and take, as well as learning new aspects of GitHub - project side, the board, switching branches, pushing to main and accepting other team members pull requests. 
 
   ## Continued development
-  challenged ourselves to continue our learning on integrating two different API's and retrieving information from 
+  For future development, our aim is to introduce a feature that provides users with healthy smoothie recipes to complement their healthy meal choices. Additionally, we will include a health and wellness blog to provide users with informative and educational articles on healthy eating and wellness.
 
   ## License
-  [![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)
-
-  ## Authors
-  * Jessie
-* Sophie
-* Ebele
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 
   ## Credits
-  no
-  
+  yes
+  * Image: Shaif Arfan
+  * Icons: Icons8
+
+  ## Authors
+* Jessie
+* Sophie
+* Ebele
 
 
   
--- index.mjs
+++ index.mjs
@@ -1,29 +1,31 @@
-import inquirer from 'inquirer';
+import inquirer from "inquirer";
 import fs from "fs/promises";
 
+
+// Function that checks for an input, and prompts the user to enter an input if none has been provided.
+// function is then used to verify the user input using the validate inquirer prompt.
 const requireInput = (input) => {
-    if (input) {
-      return true;
-    }
-    else{
-        return 'Please input the required information to proceed.';
-    }
+  if (input) {
+    return true;
+  } else {
+    return "Please input the required information to proceed.";
+  }
+};
 
-  };
 
-let {title, contentTable, description, installation, usage, screenshot, builtWith, learnt,
-    continuedDevelopment, license, authors, confirmCredits, credits} = await inquirer.prompt([
-    {
-      type: 'input',
-      name: 'title',
-      message: "What is the title of your project?",
-      validate: requireInput
-    },
-    {
-      type: 'input',
-      name: 'contentTable',
-      message: "Insert table of contents or hit ENTER to accept the default",
-      default: `- [Project Description](#project-description)
+let {title, contentTable, description, installation, usage, screenshot, builtWith, learnt, continuedDevelopment,
+  license,confirmCredits, credits, authors} = await inquirer.prompt([
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+    validate: requireInput,
+  },
+  {
+    type: "input",
+    name: "contentTable",
+    message: "Insert table of contents or hit ENTER to accept the default",
+    default: `- [Project Description](#project-description)
       - [Installation](#installation)
       - [Usage](#usage)
       - [Screenshot](#screenshot)
@@ -32,112 +34,119 @@ let {title, contentTable, description, installation, usage, screenshot, builtWit
       - [Continued development](#continued-development)
       - [License](#license)
       - [Authors](#authors)
-      - [Credits](#credits)`
-      },
-    {
-      type: 'input',
-      name: 'description',
-      message: "Write a description of your project?",
-      validate: requireInput
-      },
-      {
-        type: 'input',
-        name: 'installation',
-        message: "How can the application be installed?",
-        default: "```\n" +
-        "# Clone this repository:\n" +
-        "git clone https://github.com/KingJessie/Better_Nutrient.git\n" +
-        "cd Better_Nutrient\n" +
-        "# To open folder in VS Code\n" +
-        "code .\n" +
-        "```\n"
-      },
-      {
-        type: 'input',
-        name: 'usage',
-        message: "Write the usage of the project?",
-        default: "No usage provided"
-      },
-    {
-      type: 'list',
-      name: 'screenshot',
-      message: 'What type of screenshot would you like to add?',
-      choices: ['Image', 'Video', 'Both'],
-      filter(val) {
+      - [Credits](#credits)`,
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Write a description of your project?",
+    validate: requireInput,
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "How can the application be installed?",
+    default:
+      "```\n" +
+      "# Clone this repository:\n" +
+      "git clone https://github.com/KingJessie/Better_Nutrient.git\n" +
+      "cd Better_Nutrient\n" +
+      "# To open folder in VS Code\n" +
+      "code .\n" +
+      "```\n",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Write the usage of the project?",
+    default: "No information provided",
+  },
+  {
+    type: "list",
+    name: "screenshot",
+    message: "What type of screenshot would you like to add?",
+    choices: ["Image", "Video", "Both"],
+    filter(val) {
+      return val.toLowerCase();
+    },
+  },
+  {
+    type: "input",
+    name: "builtWith",
+    message: "What technologies did you use?",
+    default:" - [] Technologies: \n" +
+      " - [x] Semantic HTML5 markup \n" +
+      " - [x] CSS custom properties \n" +
+      " - [x] JavaScript \n" +
+      " - [x] jQuery \n" +
+      " - [x] Bootstrap \n" +
+      " - [x] Flexbox \n" +
+      " - [x] Mobile-first workflow ",
+  },
+  {
+    type: "input",
+    name: "learnt",
+    message:
+      "What did you learn from the projects and what challenges did you overcome?",
+    validate: requireInput,
+  },
+  {
+    type: "input",
+    name: "continuedDevelopment",
+    message: "Write your continued development?",
+    default: "No information provided",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Select a license badge",
+    choices: ["MIT", "ISC", "Boost"],
+    filter(val) {
+      return val.toLowerCase();
+    },
+  },
+  {
+    type: "list",
+    name: "confirmCredits",
+    message: "Do you want to provide credits?",
+    choices: ["Yes", "None"],
+    filter(val) {
         return val.toLowerCase();
       }
-    },
-    {
-        type: 'input',
-        name: 'builtWith',
-        message: 'What technologies did you use?',
-        default: " - [x] Semantic HTML5 markup \n" +
-      " - [x] CSS custom properties \n" +
-     " - [x] JavaScript \n" +
-     " - [x] jQuery \n" +
-     " - [x] Bootstrap \n" +
-     " - [x] Flexbox \n" +
-     " - [x] Mobile-first workflow " 
-
-      },
-      {
-        type: 'input',
-        name: 'learnt',
-        message: "What did you learn from the projects and what challenges did you overcome?",
-        validate: requireInput
-      },
-      {
-        type: 'input',
-        name: 'continuedDevelopment',
-        message: "Write your continued development?",
-      },
-      {
-        type: 'list',
-        name: 'license',
-        message: 'Select a license badge',
-        choices: ['MIT', 'ISC', 'Boost'],
-        filter(val) {
-          return val.toLowerCase();
-        }
-      },
-      {
-        type: 'input',
-        name: 'authors',
-        message: "Write the author(s) for the project",
-        validate: requireInput
-      },
-      {
-        type: 'list',
-        name: 'confirmCredits',
-        message: 'Do you want to provide credits?',
-        choices: ['Yes', 'No'],
-        filter(val) {
-          return val.toLowerCase();
-        }
-      },
-      {
-        type: 'input',
-        name: 'credits',
-        message: 'Please provide credits:',
-        when: (response) => response.confirmCredits === 'Yes',
+  },
+  {
+    type: "input",
+    name: "credits",
+    message: "Please provide credits:",
+    //  Confirm credits: If user choice is 'Yes,' then it asks user to submit response.
+    // A default answer is used if no information has been given.
+    // If the user chose "No," the programme would end and the word "NONE" would be shown in the credits.
+    when: (response) => response.confirmCredits === 'yes',
         default: () => 'No information submitted'
-      }
+  },
+  {
+    type: "input",
+    name: "authors",
+    message: "Write the author(s) for the project",
+    validate: requireInput,
+  },
 
-  ]);
+]);
 
 
-  function returnList(input) {
-    if (input) {
-      return input.split(",").map(item => `* ${item.trim()}`).join("\n");
-    } else {
-      return "";
-    }
+// This function converts user input to markdown by splitting, trimming (to remove whitespace),
+// and joining the strings with newline (\n) to create a formatted list.
+function returnList(input) {
+  if (input) {
+    return input
+      .split(",")
+      .map((item) => `* ${item.trim()}`)
+      .join("\n");
   }
-  
+}
 
-  
-  let readMeGenerator =  `
-  # ![screenshot](images/readme.png) **${title}** 
+let readMeGenerator = `
+  # **${title}** ✒️📃
   
   ## Table of contents
   ${contentTable}
@@ -167,40 +176,41 @@ let {title, contentTable, description, installation, usage, screenshot, builtWit
   ## License
   ${generateLicense(license)}
 
-  ## Authors
-  ${returnList(authors)}
-
   ## Credits
   ${confirmCredits}
   ${returnList(credits)}
 
+  ## Authors
+  ${returnList(authors)}
+
 
   `;
-  
-  await fs.writeFile("CreateREADME.md", readMeGenerator);
-  
-  function generateLicense(license){
-    if (license === 'mit') {
-      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
-    } else if (license === 'isc') {
-      return '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)';
-    } else {
-      return '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)';
-    }
-  };
+// Node fs (file system) module to create a new file and write the contents of the readMeGenerator variable to it.
+await fs.writeFile("CreateREADME.md", readMeGenerator);
 
-  function generateScreenshot(screenshot) {
-    if (screenshot === 'both') {
-      return '![screenshot](images/bn.gif)\n![screenshot](images/bn.png)';
-    } else if (screenshot === 'video') {
-      return '![screenshot](images/bn.gif)';
-    } else {
-      return '![screenshot](images/bn_image.png)';
-    }
-  };
+// This function generates different licence types depending on the value of the licence argument. 
+// Conditional statements provide different output according to license value which user would have selected.
+function generateLicense(license) {
+  if (license === "mit") {
+    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+  } else if (license === "isc") {
+    return "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)";
+  } else {
+    return "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
+  }
+}
 
-  
-  
-  console.log("success!");
+// This function generates a screenshot image based on its screenshot parameter.
+// Conditional statements provide different output according to screenshot value.
+function generateScreenshot(screenshot) {
+  if (screenshot === "both") {
+    return "![screenshot](images/bn.gif)\n![screenshot](images/bn.png)";
+  } else if (screenshot === "video") {
+    return "![screenshot](images/bn.gif)";
+  } else {
+    return "![screenshot](images/bn_image.png)";
+  }
+}
 
-  
+// Success is printed on the terminal when the software is complete and functional.
+console.log("success!");
